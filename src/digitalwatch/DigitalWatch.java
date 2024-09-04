a737e99619d04542f49eb13aa902ae208a9d9fc1
HaroldHoang <viethunghoang1508@gmail.com>
2024-09-04 09:34:01 +0700

    Resize frame and set layout

--- src/digitalwatch/DigitalWatch.java
+++ src/digitalwatch/DigitalWatch.java
@@ -1,18 +1,33 @@
 package digitalwatch;
 
+import java.awt.Insets;
+import java.awt.Toolkit;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class DigitalWatch extends JFrame{
     
     int WIDTH = 300;
-    int HEIGHT = 200;
-
+    int HEIGHT = 150;
+    int BUTTON_WIDTH = WIDTH / 2;
+    int BUTTON_HEIGHT = HEIGHT / 2;
+    JButton b;
+    int hour = 0, minutes = 0, seconds = 0;
     DigitalWatch(){
-        this.setSize(WIDTH, HEIGHT);
-        this.setLayout(null);
         this.setVisible(true);
+        Insets i = this.getInsets();
+        this.setSize(WIDTH + i.left + i.right, HEIGHT + i.top + i.bottom);
+        
+        b = new JButton();
+        b.setBounds(WIDTH / 4, HEIGHT / 4, BUTTON_WIDTH, BUTTON_HEIGHT);
+        this.add(b);
+        
+        this.setLayout(null);
+        this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        
+
     }
     public static void main(String[] args) {
         new DigitalWatch();
